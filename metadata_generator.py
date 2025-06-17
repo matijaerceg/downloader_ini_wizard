c3e9a5e63e85f48f46851d05e979953ddb5f1d0d
matijaerceg <matija.erceg@gmail.com>
2025-06-17 13:24:04 -0700

    Create metadata_generator.py

---
+++ metadata_generator.py
@@ -0,0 +1,9 @@
+GROUPING_RULES = {
+  "arcade_games_all": {
+    "name_for_display": "Every Individual Arcade Game",
+    "description": "Every arcade game across the major repositories.",
+    "patterns": [
+      "^_Arcade/",
+      
+    ]
+}
